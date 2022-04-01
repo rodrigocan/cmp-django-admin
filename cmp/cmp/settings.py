7916c460138bef4ffdf3b576cd093fc73b62bcbb
Rodrigo Cândido <rodrigocan@gmail.com>
2022-04-01 00:09:00 -0300

    Add Bulma CSS

--- cmp/cmp/settings.py
+++ cmp/cmp/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- cmp/cmp_admin/urls.py
+++ cmp/cmp_admin/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -7,5 +7,5 @@ urlpatterns = [
   path('', views.IndexView.as_view(), name = 'index'),
   path('ticket/<int:pk>/', views.TicketDetailView.as_view(), name = 'ticket_detail'),
   path('open_ticket/', views.OpenTicketView.as_view(), name = 'open_ticket'),
-  path('show_tickets/', views.ShowTicketsView.as_view(), name = 'show_tickets')
+  path('show_tickets/', views.ShowTicketsView.as_view(), name = 'show_tickets'),
 ]
